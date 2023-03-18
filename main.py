d8311aca0a7089c04ae2f2c4cc5ec66a5f60bd41
PYROP3 <caiotsan+github@gmail.com>
2023-03-17 21:15:16 -0300

    Initial upload

---
+++ main.py
@@ -0,0 +1,44 @@
+from http.server import BaseHTTPRequestHandler,HTTPServer
+import logging
+import requests
+
+logging.basicConfig()
+
+logger = logging.getLogger(__name__)
+logger.setLevel("ERROR")
+
+class ProxyHTTPRequestHandler(BaseHTTPRequestHandler):
+    def do_HEAD(self):
+        self.do_GET(body=False)
+    
+    def do_GET(self, body=True):
+        sent = False
+        try:
+            resp = None
+            try:
+                hostname = '200.232.172.61:8888'
+                url = 'http://{}{}'.format(hostname, self.path)
+                logger.debug(f"forward {self.path} to {url}")
+
+                resp = requests.get(url, headers=self.headers, verify=False)
+                self.send_response(resp.status_code)
+
+                for header in resp.headers:
+                    self.send_header(header, resp.headers[header])
+                self.end_headers()
+                if body:
+                    self.wfile.write(resp.content)
+                sent = True
+                return
+            finally:
+                pass
+        except IOError as e:
+            logger.error('error during proxy', exc_info=1)
+            if not sent:
+                self.send_error(404, 'error trying to proxy: {}'.format(str(e)))
+
+if __name__ == '__main__':
+    server_address = ('0.0.0.0', 80)
+    httpd = HTTPServer(server_address, ProxyHTTPRequestHandler)
+    print('http server is running')
+    httpd.serve_forever()
